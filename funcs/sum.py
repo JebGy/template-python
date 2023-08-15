f18a2f0a22a0f570856e20a5741a1b77bb5ec39b
JebGy <martinmunayco123@hotmail.com>
2023-08-15 11:35:39 -0500

    Mod back

---
+++ funcs/sum.py
@@ -0,0 +1,5 @@
+def sumarValores(lista):
+    suma = 0
+    for i in lista:
+        suma += i
+    return suma
--- server.py
+++ server.py
@@ -1,21 +1,44 @@
 import os
-from flask import Flask, send_from_directory, render_template, redirect
+from flask import Flask, jsonify, send_from_directory, render_template, redirect
+
+from funcs.sum import sumarValores
+
 
 app = Flask(__name__)
 
 port = int(os.environ.get("PORT", 5000))
 
+
 @app.route('/static/<path:path>')
 def serve_static(path):
     return send_from_directory('static', path)
 
+
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
+
+
+'''
+Comentado para que no se pueda acceder a las rutas
+'''
+@app.route('/objectJS/<valores>')
+def objectJS(valores):
+    #transform valores to list
+    valores = valores.replace("[", "")
+    valores = valores.replace("]", "")
+    valores = valores.split(",")
+    valores = [int(i) for i in valores]
+
+    return jsonify(sumarValores(valores))
+
+
+
 
 @app.route('/<path:path>')
 def all_routes(path):
     return redirect('/')
+
 
 if __name__ == "__main__":
     app.run(port=port)
